e7ae275d24e78b04abd578e56d4ac193af893e16
Awienberg <a.wienberg@hotmail.com>
2020-05-08 18:13:48 +0200

    profil & dashboard

--- repository/public/javascripts/main.js
+++ repository/public/javascripts/main.js
@@ -5,11 +5,8 @@ import { Ajax } from "./modules/Ajax.js";
 
 var maxchar = 167;
 var i = document.getElementById("yaddatxtarea");
-var c = document.getElementById("count");
+var c = document.getElementById("charcount");
 c.innerHTML = "Remaining characters: " + maxchar;
-    
-i.addEventListener("keydown",count);
-
 function count(e){
     var len =  i.value.length;
     if (len >= maxchar){
@@ -17,4 +14,19 @@ function count(e){
     } else{
        c.innerHTML = maxchar - len-1;   
     }
+       
+};
+
+i.addEventListener("keyup", count);
+
+
+function getFollowing() {
+   var following = user.following;
+   $("following").innerHTML = following.length;
 }
+
+function showstarter() {
+   getFollowing();
+}
+
+window.addEventListener('load', showstarter); // kick off JS
