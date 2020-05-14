db6a5c81ad43740b9a2193402edc89756ba80b11
mads <mads2097@gmail.com>
2020-05-14 20:08:43 +0200

    Alternative js bundle fix

--- repository/public/javascripts/bundle.js
+++ repository/public/javascripts/bundle.js
@@ -8450,7 +8450,7 @@ var signOut = /*#__PURE__*/function () {
 
           case 3:
             res = _context2.sent;
-            if (res.data.status = 'success') location.reload(true);
+            if (res.data.status = 'success') location.assign('/alt/');
             _context2.next = 10;
             break;
 
@@ -8776,7 +8776,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53886" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56666" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
