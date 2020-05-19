1b5a372e8471dd25e4bea2834ed187484e495eb9
Awienberg <a.wienberg@hotmail.com>
2020-05-19 11:35:03 +0200

    glemte lige lidt mere

--- repository/public/javascripts/darkTheme.js
+++ repository/public/javascripts/darkTheme.js
@@ -1,24 +1,26 @@
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
-const currentTheme = localStorage.getItem("theme");
+"use strict";
+import { $ } from "./modules/nQuery.js";
+import { Ajax } from "./modules/Ajax.js";
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-  }
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
 }
 
 function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-  }
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
 }
 
-toggleSwitch.addEventListener("change", switchTheme, false);
+toggleSwitch.addEventListener('change', switchTheme, false);
